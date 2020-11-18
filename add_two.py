dda0f35fca8f2b1098ccf7e83c8d877e506b5534
Kamoliddin <nabijonovkamol@gmail.com>
2020-11-18 14:07:00 +0500

    Linked list class implemented with required methods

--- add_two.py
+++ add_two.py
@@ -5,3 +5,36 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+# Linked list class implementation
+
+
+class LinkedList:
+    @classmethod
+    # Build linked_list from given list of values
+    def build_linked_list(self, values_list):
+        number = None
+        for value in values_list:
+            if number == None:
+                number = ListNode(value, None)
+            else:
+                self.get_last(number).next = ListNode(value, None)
+        return number
+
+    @classmethod
+    # Get last element of linked list
+    def get_last(self, ListNode):
+        last = ListNode
+        while (last.next):
+            last = last.next
+        return last
+
+    @classmethod
+    # Get numbers stored in linked list
+    def get_elements(self, ListNode):
+        values_list = []
+        temp = ListNode
+        while(temp):
+            values_list.append(temp.val)
+            temp = temp.next
+        return values_list
