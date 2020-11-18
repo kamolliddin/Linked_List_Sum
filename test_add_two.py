2919581645b0bcd869a1efc5e291f9ea0b7d3b98
Kamoliddin <nabijonovkamol@gmail.com>
2020-11-18 14:09:21 +0500

    Test runner test_sum implemented

--- test_add_two.py
+++ test_add_two.py
@@ -25,3 +25,14 @@ expected_outputs = [
     [7, 0, 8, 1, 3, 5, 6],
     [7, 0, 8, 1, 3, 5, 6]
 ]
+
+
+class TestAddTwo(unittest.TestCase):
+    # Test for correct results
+    def test_sum(self):
+        for i in range(len(inputs_for_list1)):
+            l1 = LinkedList.build_linked_list(inputs_for_list1[i])
+            l2 = LinkedList.build_linked_list(inputs_for_list2[i])
+            expected_sum = LinkedList.build_linked_list(expected_outputs[i])
+            self.assertAlmostEqual(LinkedList.get_elements(
+                addTwoNumbers(l1, l2)), LinkedList.get_elements(expected_sum))
